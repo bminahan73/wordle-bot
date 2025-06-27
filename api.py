6fa41e319b5ecdf9e58c0a39f5d1d86bb9d62d7a
Ben Minahan <bminahan73@gmail.com>
2025-06-27 08:05:49 -0500

    adding uvicorn

--- api.py
+++ api.py
@@ -7,11 +7,13 @@ import datetime
 with open("results.json", "r") as f:
     results = json.load(f)
 
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/api/v1/results/today")
+api_app = FastAPI(title="api app")
+@api_app.get("/results/today")
 def get_todays_results():
     todays_solution = requests.get(url=f"https://www.nytimes.com/svc/wordle/v2/{str(datetime.date.today())}.json").json()["solution"]
     return next((x for x in results if x["solution"] == todays_solution), None)
+
+app = FastAPI()
+
+app.mount("/api/v1", api_app)
+app.mount("/", StaticFiles(directory="static",html = True), name="static")

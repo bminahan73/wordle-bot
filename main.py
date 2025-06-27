d6231fc71c0dabe37b0efb80a0ae5ac9b5566930
Ben Minahan <bminahan73@gmail.com>
2025-06-27 15:25:14 -0500

    changed default entrypoint

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ app.mount("/api/v1", api_app)
 app.mount("/", StaticFiles(directory="static",html = True), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=443, ssl_keyfile='key.pem', ssl_certfile='cert.pem')
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
